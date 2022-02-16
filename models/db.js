d64b6b599873a3fbc5119848660d3405df303e9c
Aluno-lucas-Antonio <lucasbielemann@gmail.com>
2022-02-16 16:56:32 -0300

    EJS framerworks web

--- models/db.js
+++ models/db.js
@@ -1,9 +1,9 @@
 const Sequelize = require("sequelize")
 
 //conexão com o banco de dados
-const sequelize = new Sequelize('cadastro','root','Caraluma@805',{
+const sequelize = new Sequelize('cadastro','postgres','15080429',{
     host: "localhost",  // local do banco
-    dialect: "mysql"    // tipo do banco de dados
+    dialect: "postgres"    // tipo do banco de dados
 }) 
 
 sequelize.authenticate().then(()=>{// verificando se a conexão foi feita.
